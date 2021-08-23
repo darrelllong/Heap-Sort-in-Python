b363cdea786cab94868326480076f1a36d83efd5
Eugene Chou <euchou@ucsc.edu>
2021-08-22 23:30:30 -0700

    fix off-by-one and add assert

--- heapsort.py
+++ heapsort.py
@@ -5,21 +5,19 @@
 def maxChild(seq, first, last):
     left  = 2 * first
     right = left + 1;
-    return right if right < last and seq[right] > seq[left] else left
+    return right if right <= last and seq[right] > seq[left] else left
 
 def fixHeap(seq, first, last):
     found  = False
     father = first
     great  = maxChild(seq, father, last)
-    root   = seq[first]
     while father <= last // 2 and not found:
-        if root < seq[great]:
-             seq[father] = seq[great]
-             father = great
-             great = maxChild(seq, father, last)
+        if seq[father] < seq[great]:
+            seq[father], seq[great] = seq[great], seq[father]
+            father = great
+            great = maxChild(seq, father, last)
         else:
-             found = True
-    seq[father] = root
+            found = True
 
 def buildHeap(seq, last):
     for father in range(last // 2, -1, -1):
@@ -27,9 +25,9 @@ def buildHeap(seq, last):
 
 def heapSort(seq):
     first = 0
-    last  = len(seq)
+    last  = len(seq) - 1
     buildHeap(seq, last)
-    for leaf in range(last - 1, -1, -1):
+    for leaf in range(last, 0, -1):
         seq[first], seq[leaf] = seq[leaf], seq[first],
         fixHeap(seq, first, leaf - 1)
 
